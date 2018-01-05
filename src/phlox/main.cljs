5b25240f53825cf48eb7f2cf1cff4c300f8d57cb
jiyinyiyong <jiyinyiyong@gmail.com>
2018-01-06 00:50:50 +0800

    pixi is available in shadow-cljs

--- src/phlox/main.cljs
+++ src/phlox/main.cljs
@@ -2,11 +2,12 @@
 (ns phlox.main
   (:require [phlox.updater :refer [updater]]
             [phlox.schema :as schema]
-            [verbosely.core :refer [log!]]))
+            [verbosely.core :refer [log!]]
+            ["pixi.js" :as pixi]))
 
 (defn dispatch! [op op-data] )
 
-(defn main! [] (println "App started."))
+(defn main! [] (println "App started.") (.log js/console pixi))
 
 (def mount-target (.querySelector js/document ".app"))
 
