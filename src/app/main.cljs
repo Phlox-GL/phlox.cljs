51898db9c0bc43535e7f20bc99f4719d53256361
jiyinyiyong <jiyinyiyong@gmail.com>
2019-01-24 02:19:41 +0800

    include PIXI back in project

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,6 +1,18 @@
 
-(ns app.main )
+(ns app.main (:require ["pixi.js" :as PIXI]))
 
-(defn main! [] (println "App Started"))
+(defn main! []
+  (println "App Started")
+  (js/console.log PIXI)
+  (let [app (PIXI/Application. 800 600 (clj->js {:background-color 0x1099bb}))
+        g (PIXI/Graphics.)]
+    (.appendChild js/document.body (.-view app))
+    (-> g
+        (.beginFill 0xff3300)
+        (.lineStyle 4 0xffd900 1)
+        (.moveTo 50 50)
+        (.lineTo 100 100)
+        (.endFill))
+    (.addChild (.-stage app) g)))
 
 (defn reload! [] (println "Code updated"))
