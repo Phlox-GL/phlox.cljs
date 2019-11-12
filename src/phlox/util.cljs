21ef2f259baebd375389e5c02cbdb18c28397270
jiyinyiyong <jiyinyiyong@gmail.com>
2019-11-13 01:10:32 +0800

    tree traversing code for updating(incomplete)

--- src/phlox/util.cljs
+++ src/phlox/util.cljs
@@ -7,5 +7,9 @@
 
 (defn hslx [h s l] (-> color (.hsl h s l) (.rgbNumber)))
 
+(defn index-items [xs] (->> xs (map-indexed (fn [idx x] [idx x]))))
+
+(defn remove-nil-values [dict] (->> dict (filter (fn [[k v]] (some? v)))))
+
 (defn use-number [x]
   (if (and (number? x) (not (js/isNaN x))) x (do (js/console.error "Invalid number:" x) 0)))
