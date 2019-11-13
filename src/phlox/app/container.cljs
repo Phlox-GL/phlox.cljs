f48a70e87380036950b357bdfff5338c275bd123
jiyinyiyong <jiyinyiyong@gmail.com>
2019-11-14 00:50:23 +0800

    trying very naive interaction, click and move

--- src/phlox/app/container.cljs
+++ src/phlox/app/container.cljs
@@ -2,19 +2,22 @@
 (ns phlox.app.container
   (:require [phlox.core :refer [render-tag]] [phlox.util :refer [hslx]]))
 
-(defn comp-container []
+(defn comp-container [store]
+  (println "store" store)
   {:name :container,
    :phlox-node :component,
-   :args [],
+   :args [store],
    :tree (render-tag
           :container
           {:options {:x 100, :y 0}}
           (render-tag
            :circle
-           {:options {:x 100, :y 100, :radius 40},
+           {:options {:x (+ 80 (:x store)), :y 100, :radius 40},
             :line-style {:width 2, :color (hslx 0 80 50), :alpha 1},
             :fill (hslx 160 80 70),
-            :on {:mousedown (fn [] (println "circle click"))}})
+            :on {:mousedown (fn [event dispatch!]
+                   (js/console.log "circle click" event)
+                   (dispatch! :add-x "a"))}})
           (render-tag
            :rect
            {:options {:x 200, :y 100, :width 50, :height 50},
