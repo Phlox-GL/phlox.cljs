ea590e57e264c91b600f80f053744aee94f0fd3c
jiyinyiyong <jiyinyiyong@gmail.com>
2020-11-20 15:28:05 +0800

    fix typo; upgrade deps

--- macros/phlox/core.clj
+++ macros/phlox/core.clj
@@ -5,7 +5,7 @@
   (assert (symbol? x) "1st argument should be a symbol")
   (assert (coll? params) "2nd argument should be a collection")
   (assert (some? (last body)) "defcomp should return something")
-  (let [com-helper (gensym (str x "-helper-"))]
+  (let [comp-helper (gensym (str x "-helper-"))]
     `(do
-       (defn ~com-helper [~@params] ~@body)
-       (defn ~x [~@params] (phlox.core/call-comp-helper ~com-helper [~@params])))))
+       (defn ~comp-helper [~@params] ~@body)
+       (defn ~x [~@params] (phlox.core/call-comp-helper ~comp-helper [~@params])))))
